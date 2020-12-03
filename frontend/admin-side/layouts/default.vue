0ed2c9991e203a5d574b10423bd7a13fcb18311d
Shovon Das <shovondas26@gmail.com>
2020-12-03 16:33:04 +0600

    layout done

--- frontend/admin-side/layouts/default.vue
+++ frontend/admin-side/layouts/default.vue
@@ -1,62 +1,242 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+<div class="main-wrapper">
+	
+		<!-- Header -->
+		<div class="header">
+			<div class="header-left"> 
+				<a href="index-2.html" class="logo logo-small">
+					<img src="img/logo-icon.png" alt="Logo" width="30" height="30">
+				</a>
+			</div>
+			<a href="javascript:void(0);" id="toggle_btn">
+				<i class="fas fa-align-left"></i>
+			</a>
+			<a class="mobile_btn" id="mobile_btn" href="javascript:void(0);">
+				<i class="fas fa-align-left"></i>
+			</a>
+			
+			<ul class="nav user-menu">
+				<!-- Notifications -->
+				<li class="nav-item dropdown noti-dropdown">
+					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+						<i class="far fa-bell"></i>  <span class="badge badge-pill"></span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right notifications">
+						<div class="topnav-dropdown-header">
+							<span class="notification-title">Notifications</span>
+							<a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+						</div>
+						<div class="noti-content">
+							<ul class="notification-list">
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="" src="img/provider/provider-01.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Thomas Herzberg have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">15 Sep 2020 10:20 PM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="" src="img/provider/provider-02.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Matthew Garcia have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">13 Sep 2020 03:56 AM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="" src="img/provider/provider-03.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Yolanda Potter have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">12 Sep 2020 09:25 PM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="User Image" src="img/provider/provider-04.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Ricardo Flemings have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">11 Sep 2020 06:36 PM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="User Image" src="img/provider/provider-05.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Maritza Wasson have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">10 Sep 2020 08:42 AM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="User Image" src="img/provider/provider-06.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Marya Ruiz have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">9 Sep 2020 11:01 AM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+								<li class="notification-message">
+									<a href="admin-notification.html">
+										<div class="media">
+											<span class="avatar avatar-sm">
+												<img class="avatar-img rounded-circle" alt="User Image" src="img/provider/provider-07.jpg">
+											</span>
+											<div class="media-body">
+												<p class="noti-details">
+													<span class="noti-title">Richard Hughes have been subscribed</span>
+												</p>
+												<p class="noti-time">
+													<span class="notification-time">8 Sep 2020 06:23 AM</span>
+												</p>
+											</div>
+										</div>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div class="topnav-dropdown-footer">
+							<a href="admin-notification.html">View all Notifications</a>
+						</div>
+					</div>
+				</li>
+				<!-- /Notifications -->
+				
+				<!-- User Menu -->
+				<li class="nav-item dropdown">
+					<a href="javascript:void(0)" class="dropdown-toggle user-link  nav-link" data-toggle="dropdown">
+						<span class="user-img">
+							<img class="rounded-circle" src="img/user.jpg" width="40" alt="Admin">
+						</span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right">
+						<a class="dropdown-item" href="admin-profile.html">Profile</a>
+						<a class="dropdown-item" href="login.html">Logout</a>
+					</div>
+				</li>
+				<!-- /User Menu -->
+				
+			</ul>
+		</div>
+		<!-- /Header -->
+		
+		<!-- Sidebar -->
+		<div class="sidebar" id="sidebar">
+			<div class="sidebar-logo">
+				<a href="index-2.html">
+					<img src="img/logo-icon.png" class="img-fluid" alt="">
+				</a>
+			</div>
+			<div class="sidebar-inner slimscroll">
+				<div id="sidebar-menu" class="sidebar-menu">
+					<ul>
+						<li class="active">
+							<a href="index-2.html"><i class="fas fa-columns"></i> <span>Dashboard</span></a>
+						</li>
+						<li>
+							<a href="categories.html"><i class="fas fa-layer-group"></i> <span>Categories</span></a>
+						</li>
+						<li>
+							<a href="subcategories.html"><i class="fab fa-buffer"></i> <span>Sub Categories</span></a>
+						</li>
+						<li>
+							<a href="service-list.html"><i class="fas fa-bullhorn"></i> <span> Services</span></a>
+						</li>
+						<li>
+							<a href="total-report.html"><i class="far fa-calendar-check"></i> <span> Booking List</span></a>
+						</li>
+						<li>
+							<a href="payment_list.html"><i class="fas fa-hashtag"></i> <span>Payments</span></a>
+						</li>
+						<li>
+							<a href="ratingstype.html"><i class="fas fa-star-half-alt"></i> <span>Rating Type</span></a>
+						</li>
+						<li>
+							<a href="review-reports.html"><i class="fas fa-star"></i> <span>Ratings</span></a>
+						</li>
+						<li>
+							<a href="subscriptions.html"><i class="far fa-calendar-alt"></i> <span>Subscriptions</span></a>
+						</li>
+						<li>
+							<a href="wallet.html"><i class="fas fa-wallet"></i> <span> Wallet</span></a>
+						</li>
+						<li>
+							<a href="service-providers.html"><i class="fas fa-user-tie"></i> <span> Service Providers</span></a>
+						</li>
+						<li>
+							<a href="users.html"><i class="fas fa-user"></i> <span>Users</span></a>
+						</li>
+						<li>
+							<a href="settings.html"><i class="fas fa-cog"></i> <span> Settings</span></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- /Sidebar -->
+		
+		<div class="page-wrapper">
+			<nuxt />
+		</div> 
+	</div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
