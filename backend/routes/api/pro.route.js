4be57ad02839fad45490dfa238bd1ff7de008f17
AndiT Service <webguru071@gmail.com>
2020-12-08 10:26:00 +0600

    get all plan by pro

--- backend/routes/api/pro.route.js
+++ backend/routes/api/pro.route.js
@@ -1,6 +1,10 @@
 const express = require('express') 
 const router = express.Router() 
 
+// Get All Plan---------------------------
+const Plan = require('../../controllers/admin/plan.controller')
+router.get('/plans', Plan.getAllPlansByUser)
+
 // Pro Controller--------------------------
 const Pro = require('../../controllers/pro/pro.controller') 
 router.post('/signup', Pro.signUpPro) 
