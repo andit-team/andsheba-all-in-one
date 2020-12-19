5867dda23fa84004f561bebf93038a5c131f6840
Shovon Das <shovondas26@gmail.com>
2020-12-19 12:38:52 +0600

    search bar new

--- frontend/client-side/src/components/sliders/NearestTop.vue
+++ frontend/client-side/src/components/sliders/NearestTop.vue
@@ -1,67 +1,75 @@
 <template>
-    <div class="full-width inline column justify-center items-center content-center q-pa-xl bg-grey-2">
-      <p class="text-h5 q-mb-xl">আপনার এলাকার জনপ্রিয় সেবা সমুহ</p>
-      <div class="row justify-center items-center q-gutter-md">
-      <q-card class="col-md-2 col-sm-3 col-xs-5 my-card">
-        <img src="https://andsheba.com/_nuxt/img/3.47d4a83.jpg">
-
-        <q-card-section>
-          <div class="text-h6">কুকুর প্রশিক্ষণ</div>
-          <div class="text-subtitle2">by Shovon Das</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none" v-if="$q.screen.gt.sm">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio dolor, neque quibusdam impedit
-        </q-card-section>
-      </q-card>
-
-      <q-card class="col-md-2 col-sm-3 col-xs-5 my-card">
-          <img src="https://andsheba.com/_nuxt/img/4.4bfd504.jpg">
-
-          <q-card-section>
-            <div class="text-h6">ঘর পরিস্কার</div>
-            <div class="text-subtitle2">by Kajer Bua</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none" v-if="$q.screen.gt.sm">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio dolor, neque quibusdam impedit
-          </q-card-section>
-        </q-card>
-        <q-card class="col-md-2 col-sm-3 col-xs-5 my-card">
-          <img src="https://andsheba.com/_nuxt/img/5.f9cb0c7.jpg">
-
-          <q-card-section>
-            <div class="text-h6">রাজ মিস্ত্রি</div>
-            <div class="text-subtitle2">by Shariful Islam</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none" v-if="$q.screen.gt.sm">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio dolor, neque quibusdam impedit
-          </q-card-section>
-        </q-card>
-        <q-card class="col-md-2 col-sm-3 col-xs-5 my-card">
-          <img src="https://andsheba.com/_nuxt/img/6.c99fae2.jpg">
-
-          <q-card-section>
-            <div class="text-h6">সোলার মেরামত</div>
-            <div class="text-subtitle2">by Shahajalal</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none" v-if="$q.screen.gt.sm">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio dolor, neque quibusdam impedit
-          </q-card-section>
-        </q-card>
-      </div>
+    <div class="question_area">
+        <div class="container">
+            <div class="title q-pa-md"> আপনার এলাকার জনপ্রিয় সেবা সমুহ  </div>
+            <VueSlickCarousel v-bind="slideSetting" class="question-carousel">
+                <div>
+                    <div class="block q-ma-md">
+                        <img class="full-width" src="https://andsheba.com/_nuxt/img/3.47d4a83.jpg">
+                    </div>
+                </div>
+                <div>
+                    <div class="block q-ma-md">
+                        <img class="full-width" src="https://andsheba.com/_nuxt/img/4.4bfd504.jpg">
+                    </div>
+                </div>
+                <div>
+                    <div class="block q-ma-md">
+                        <img class="full-width" src="https://andsheba.com/_nuxt/img/5.f9cb0c7.jpg">
+                    </div>
+                </div>
+                <div>
+                    <div class="block q-ma-md">
+                        <img class="full-width" src="https://andsheba.com/_nuxt/img/6.c99fae2.jpg">
+                    </div>
+                </div>
+            </VueSlickCarousel>
+        </div>
     </div>
 
-  
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+
 export default {
-  name: "NearestTop"
-};
+    name: "Questions",
+    components: { VueSlickCarousel },
+    data() {
+        return {
+            slide: 1,
+            slideSetting : {
+                "dots": false,
+                "arrows": true,
+                "focusOnSelect": true,
+                "infinite": true,
+                "slidesToShow": 3,
+                "slidesToScroll": 3,
+                "touchThreshold": 5
+            }
+        }
+    }
+}
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss">
+.question_area {
+    background: #f5f5f5;
+    padding-top: 70px;
+    padding-bottom: 130px;
+    .title {
+        font-size: 30px;
+        font-weight: 500;
+    }
+    .question-carousel {
+        padding-top: 20px;
+        img {
+            border-radius: 5px;
+        }
+    }
+}
 </style>
--- frontend/client-side/src/pages/Index.vue
+++ frontend/client-side/src/pages/Index.vue
@@ -6,59 +6,34 @@
                 <div id="parent" class="full-width row">
                     <!-- `Banner Left Search -->
                     <div
-                        :class="[$q.screen.gt.sm ? 'q-px-md' : 'q-mt-xl q-mb-xl q-mx-lg', 'flex column col-md-7 col-sm-12 justify-center' ]">
+                        :class="[$q.screen.gt.sm ? 'q-px-md' : 'q-mt-xl q-mb-xl q-mx-lg', 'flex column col-md-8 col-sm-12 justify-center' ]">
                         <p :class="[$q.screen.gt.sm ? 'text-h5' : 'text-h6 text-center']">আপনার স্থানীয় পেশাদারদের
                             খুঁজুন</p>
                         <q-form @submit="onSubmit">
                             <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-3">
-                                    <q-select
-                                        square outlined
-                                        v-model="category"
-                                        :options="categories"
-                                        option-value="_id"
-                                        option-label="name"
-                                        label="Category"
-                                        @input="handleCategorySelect"
-                                        lazy-rules
-                                        :rules="[
-                                        val => (val.name !== '') || 'Please Select a category',
-                                    ]"
-                                    />
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-3 q-mb-md">
-                                    <q-select
-                                        square outlined
-                                        v-model="sub_category"
-                                        :options="sub_categories"
-                                        option-value="_id"
-                                        option-label="name"
-                                        label="Sub Category"
-                                    />
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-3">
-                                    <q-input
-                                        square outlined
-                                        v-model="location"
-                                        class="hp-places-autocomplete"
-                                        :rules="[
-                                        val =>(val !== null) || 'Please Select Location',
-                                    ]">
-                                        <template v-slot:default>
-                                            <gmap-autocomplete
-                                                class="autocomplete-search"
-                                                placeholder="আপনার অবস্থান *"
-                                                @place_changed="setPlace">
-                                            </gmap-autocomplete>
+                                <div class="col-xs-12 col-sm-12 col-md-5">
+                                    <q-input filled v-model="category" label="What are you looking for?"  >
+                                        <template v-slot:prepend>
+                                        <q-icon name="search" />
                                         </template>
                                     </q-input>
                                 </div>
 
-                                <div class="col-xs-12 col-sm-12 col-md-3">
+                                <div class="col-xs-12 col-sm-12 col-md-5">
+                                   <div class="col-xs-12 col-sm-12 col-md-3">
+                                    <q-input filled v-model="location"  label="Your location">
+                                        <template v-slot:append>
+                                        <q-icon name="gps_fixed" />
+                                        </template>
+                                        <template v-slot:prepend>
+                                        <q-icon name="near_me" />
+                                        </template>
+                                    </q-input>
+                                </div>
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-2">
                                     <q-btn color="teal"  :class="$q.screen.gt.sm ? '' : 'full-width text-center'" type="submit" style="padding: 10px">
-                                        <q-icon left size="2em" name="search"/>
                                         <div>খুঁজুন</div>
                                     </q-btn>
                                 </div>
@@ -66,7 +41,7 @@
                         </q-form>
                     </div>
                     <!-- Banner Right Image -->
-                    <div class="col-md-5 col-sm-12" v-if="$q.screen.gt.sm">
+                    <div class="col-md-4 col-sm-12" v-if="$q.screen.gt.sm">
                         <img src="https://andsheba.com/_nuxt/img/2.5d1171b.png" alt=""/>
                     </div>
                 </div>
@@ -103,15 +78,13 @@ export default {
     },
     data() {
         return {
-            category: {
-                name: ''
-            },
+            category: '',
             sub_category: {
                 name: ''
             },
             longitude: '',
             latitude: '',
-            location: null,
+            location: '',
         }
     },
     created() {
