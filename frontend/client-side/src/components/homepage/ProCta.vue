95b4156334e97b13da97cb0347b62940923bd5ea
Md. A. Razzak (Tuhin) <tuhin1714@cseku.ac.bd>
2020-12-19 13:01:35 +0600

    Pro CTA

--- frontend/client-side/src/components/homepage/ProCta.vue
+++ frontend/client-side/src/components/homepage/ProCta.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="pro_banner">
         <div class="container">
-            <p class="text-h5 q-mb-sm">Become a Pro</p>
-            <p style="max-width: 800px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu scelerisque eros. Cras a tellus sit amet tortor bibendum elementum vel in odio. Ut iaculis feugiat quam sit amet mollis. Proin aliquet venenatis vulputate. Nam at tempor massa, et elementum dui. Quisque non auctor arcu. Vivamus venenatis bibendum augue. Nunc aliquam ante non lorem suscipit, ut fringilla turpis dignissim. Nunc blandit, massa vel commodo gravida, dui nibh vehicula erat, ut varius risus quam id quam. Nam nisi ligula, ornare eu auctor vel, rhoncus at massa. Fusce interdum urna libero, sed pellentesque metus pellentesque vitae. Donec consectetur egestas quam, sed gravida sem volutpat ut. Fusce a faucibus felis.</p>
-            <q-btn class="q-mt-sm q-pa-sm text-white bg-primary" to="/user/register">Become a pro</q-btn>
+            <p class="text-h6" style="margin-bottom: 5px">We are spreading Day by Day</p>
+            <p class="text-h4 q-mb-sm text-primary">Join our friendly community</p>
+            <p style="max-width: 600px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu scelerisque eros. Cras a tellus sit amet tortor bibendum elementum vel in odio.</p>
+            <q-btn class="q-mt-sm q-pa-sm text-white bg-primary pro_button" to="/user/register">Become a pro</q-btn>
         </div>
     </div>
 </template>
@@ -17,6 +18,15 @@ export default {
 <style lang="scss" scoped>
     .pro_banner {
         background: #fff;
+        padding: 75px 0;
+    }
+    .container {
+        position: relative;
+    }
+    .pro_button {
+        position: absolute;
+        right: 10%;
+        top: 30%;
     }
 
 </style>
