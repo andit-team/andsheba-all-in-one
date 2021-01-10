4689c40f4936153734016e672c644d6d139f5dd0
AndiT Service <webguru071@gmail.com>
2021-01-10 10:02:55 +0600

    auth changed

--- backend/routes/api/agent.route.js
+++ backend/routes/api/agent.route.js
@@ -10,12 +10,12 @@ router.post('/verify', Agent.verifyAgent)
 
 // Service Controller -----------------------------
 const Service = require('../../controllers/pro/service.controller')
-router.get('/service', Auth.user, Service.findAllServicesByAgent)
-router.put('/service/:_id', Auth.user, Service.updateServiceMessageByAgent)
+router.get('/service', Auth.agent, Service.findAllServicesByAgent)
+router.put('/service/:_id', Auth.agent, Service.updateServiceMessageByAgent)
 
 // Order Controller------------------------
 const Order = require('../../controllers/customer/order.controller')
-router.get('/orders', Auth.user, Order.getAllOrdersByAgent)
+router.get('/orders', Auth.agent, Order.getAllOrdersByAgent)
 router.get('/order', Order.getOneOrder)
 
 
--- backend/routes/api/pro.route.js
+++ backend/routes/api/pro.route.js
@@ -4,28 +4,27 @@ const Auth = require('../../auth/auth')
 
 // Get All Plan---------------------------
 const Plan = require('../../controllers/admin/plan.controller')
-router.get('/plans', Plan.getAllPlansByUser)
+router.get('/plans', Plan.getAllPlansBypro)
 
 // Pro Controller--------------------------
 const Pro = require('../../controllers/pro/pro.controller') 
 router.post('/signup', Pro.signUpPro) 
 router.post('/login', Pro.login) 
 router.post('/verify', Pro.verifyPro)
-router.put('/profile', Auth.user, Pro.updateProfile)
-//router.put('/profile-picture', Auth.user, Pro.updateProfilePicture)
+router.put('/profile', Auth.pro, Pro.updateProfile)
 
 // Service Controller-----------------------
 const Service = require('../../controllers/pro/service.controller')
-router.post('/service', Auth.user, Service.addService)
-router.get('/services', Auth.user, Service.findAllServices)
-router.get('/service/:_id', Auth.user, Service.getOneService)
-router.put('/service-status/:_id', Auth.user, Service.updateServiceStatus)
+router.post('/service', Auth.pro, Service.addService)
+router.get('/services', Auth.pro, Service.findAllServices)
+router.get('/service/:_id', Auth.pro, Service.getOneService)
+router.put('/service-status/:_id', Auth.pro, Service.updateServiceStatus)
 
 // Order Controller------------------------
 const Order = require('../../controllers/customer/order.controller')
-router.get('/orders', Auth.user, Order.getAllOrdersByPro)
+router.get('/orders', Auth.pro, Order.getAllOrdersByPro)
 router.get('/order', Order.getOneOrder)
-router.put('/order-status/:_id', Auth.user, Order.updateOrderStatusByPro)
+router.put('/order-status/:_id', Auth.pro, Order.updateOrderStatusByPro)
 
 // Export the Router
 module.exports = router 
