efe02168983c760f48b5ac1a2f7b5386209b973c
Shovon Das <shovondas26@gmail.com>
2021-01-12 12:21:53 +0600

    my services design

--- frontend/client-side/src/pages/agent/Services.vue
+++ frontend/client-side/src/pages/agent/Services.vue
@@ -1,23 +1,24 @@
 <template>
-  <div>
-    <div class="text-h5 q-ma-lg">Services in my Area</div>
+  <q-card class="fit q-mx-md" >
+    <q-card-section class="bg-secondary text-white text-center">
+        <div class="text-h5">Services in my Area</div>
+    </q-card-section>
     <q-tabs
       v-model="tab"
       dense
-      class="text-grey q-ma-sm"
+      class="fixed-height bg-grey-2 text-grey-7 q-pa-sm"
       active-color="primary"
       indicator-color="primary"
-      align="left"
-      narrow-indicator
+      align="center"
     >
-      <q-tab name="active" :label="`Active Services ( ${active.length} )`" class="text-success"/>
-      <q-tab name="inactive" :label="`Inactive Services ( ${inactive.length} )`" class="text-warning" />
-      <q-tab name="pending" :label="`Pending Services ( ${pending.length} )`" class="text-secondary"/>
+      <q-tab name="active" :label="`Active ( ${active.length} )`" class="text-green"/>
+      <q-tab name="inactive" :label="`Inactive ( ${inactive.length} )`" class="text-muted" />
+      <q-tab name="pending" :label="`Pending ( ${pending.length} )`" class="text-red"/>
     </q-tabs>
 
     <q-tab-panels v-model="tab" animated>
       <q-tab-panel name="active">
-        <div class="row q-ma-sm">
+        <div class="row">
           <div
             v-for="(service, index) in active"
             :key="index"
@@ -31,7 +32,7 @@
       </q-tab-panel>
 
       <q-tab-panel name="inactive">
-        <div class="row q-ma-sm">
+        <div class="row">
           <div
             v-for="(service, index) in inactive"
             :key="index"
@@ -43,7 +44,7 @@
       </q-tab-panel>
 
       <q-tab-panel name="pending">
-        <div class="row q-ma-sm">
+        <div class="row">
           <div
             v-for="(service, index) in pending"
             :key="index"
@@ -54,7 +55,7 @@
         </div>
       </q-tab-panel>
     </q-tab-panels>
-  </div>
+  </q-card>
 </template>
 
 <script>
